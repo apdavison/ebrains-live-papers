0adc5674ef84729f8dc8bc4b0f0e833e16f24649
Andrew Davison <andrew.davison@cnrs.fr>
2024-04-19 17:31:22 +0200

    Update the staging version to be deployed on kubernetes (no need for SSL handling within container), and to use the live papers API server running on kubernetes.

--- apps/live-paper-builder/src/globals-staging.js
+++ apps/live-paper-builder/src/globals-staging.js
@@ -2,7 +2,7 @@
 export const livePaperPlatformUrl = "/";
 export const livePaperBuilderUrl = "/builder";
 export const livePaperDocsUrl = "/docs";
-export const baseUrl = "https://validation-staging.brainsimulation.eu";
+export const baseUrl = "https://live-papers-api.apps.tc.humanbrainproject.eu";
 export const mc_baseUrl = "https://model-catalog-dev.brainsimulation.eu";
 export const nar_baseUrl =
   "https://neural-activity-resource.brainsimulation.eu";
